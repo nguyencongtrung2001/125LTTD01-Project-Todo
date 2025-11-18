d2266d8181e133c248e05fc2c466d699c359f831
AnhPhan <angphan5@gmail.com>
2025-11-18 22:56:08 +0700

    Hoàn thiện Reminder, Calendar, Birthday, Statistics + UI

---
+++ app/src/main/java/com/example/projecttodo/ReminderAdapter.java
@@ -0,0 +1,47 @@
+package com.example.projecttodo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHolder> {
+
+    private List<String> reminderList;
+
+    public ReminderAdapter(List<String> reminderList) {
+        this.reminderList = reminderList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(reminderList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return reminderList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(android.R.id.text1);
+        }
+    }
+}
