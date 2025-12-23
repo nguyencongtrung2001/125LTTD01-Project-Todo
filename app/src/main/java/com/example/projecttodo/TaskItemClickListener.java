a65334efc8168fa7cb649ccdb16c673856e31f68
BuiXuanVan294 <22115053122147@sv.ute.udn.vn>
2025-12-23 14:04:39 +0700

    Revert "Merge pull request #37 from nguyencongtrung2001/vinh"
    This reverts commit 744b6720b4c48088bdc40eb0ac0b85c5a0ebe9cd, reversing
    changes made to e59059453ccdbdda1d569d7a0bb44e5401d6be3d.

--- app/src/main/java/com/example/projecttodo/TaskItemClickListener.java
+++ app/src/main/java/com/example/projecttodo/TaskItemClickListener.java
@@ -1,8 +1,6 @@
 package com.example.projecttodo;
 
-
 public interface TaskItemClickListener {
-    void onTaskClick(Task task);
-    void onEditTask(Task task);
-    void onSelectionModeChange(boolean isSelecting);
+    void onTaskClick(Task task); // Sự kiện click để xem chi tiết
+    void onSelectionModeChange(boolean isSelecting); // Sự kiện khi chọn nhiều
 }
